6ee16b1a394cdbe56c36d83938a0c8a091a16288
andymurh <andymurh@gmail.com>
2021-08-08 13:11:33 +0200

    Kafka HW. Final solution (for now).

--- README.md
+++ README.md
@@ -10,19 +10,31 @@ Before starting the assignment please start Apache Kafka cluster up (you can use
 
 ### Assignment
 
-Create an application which reads the data from csv data file (https://www.kaggle.com/sootersaalu/amazon-top-50-bestselling-books-2009-2019) and loads them into abovementioned topic in JSON format.
-After that it should read the last 5 records of each partition from the topic (last means with the greatest offset value). While reading from the topic you're allowed to keep not more than 15 records at a time.
+Create an application which reads the data from csv data file (https://www.kaggle.com/sootersaalu/amazon-top-50-bestselling-books-2009-2019) and loads them into above mentioned topic in JSON format.
+After that, it should read the last 5 records of each partition from the topic (last means with the greatest offset value). While reading from the topic, you're allowed to keep not more than 15 records at a time.
 
 ### Solution
 
-Can be found in ```TBD```
+Can be found here ```com.andymur.ot.ecospark.KafkaHomeWorkRunner```
 
 ### How to check, compile, build & run
 
 #### Checkout
 
+Solution stored in the git repo. For checkout simply do this
+
+```git clone -b fifth_home_work_kafka https://github.com/andymur/ot-eco-spark.git```
+
+Or go and check on github: [link](https://github.com/andymur/ot-eco-spark/tree/fifth_home_work_kafka)
+
 #### Compile & Run
 
-```./kafka-topics.sh --bootstrap-server localhost:29092 --create --topic books --if-not-exists --partitions 3```
+We can compile & run our solution with a help from SBT. Just run following commands from the project's directory.
 
-#### Build JAR file & Run
+* ```sbt compile```
+* ```sbt run```
+
+You can also run ```com.andymur.ot.ecospark.KafkaHomeWorkRunner``` from IDE.
+
+Just not forget to start up the Apache Kafka broker with docker-compose and create appropriate topic with command below:
+```./kafka-topics.sh --bootstrap-server localhost:29092 --create --topic books --if-not-exists --partitions 3```
--- src/main/scala/com/andymur/ot/ecospark/KafkaHomeWorkRunner.scala
+++ src/main/scala/com/andymur/ot/ecospark/KafkaHomeWorkRunner.scala
@@ -1,17 +1,19 @@
 package com.andymur.ot.ecospark
 
 import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.jawn.decode
 import io.circe.syntax.EncoderOps
 import io.circe.{Decoder, Encoder}
 import org.apache.commons.csv.{CSVFormat, CSVRecord}
-import org.apache.kafka.clients.consumer.{Consumer, ConsumerConfig, KafkaConsumer}
-import org.apache.kafka.clients.producer.{KafkaProducer, Producer, ProducerRecord}
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
-import java.io.{FileReader, Reader}
+import java.io.FileReader
 import java.time.Duration
 import java.util.{Collections, Properties}
 import scala.collection.mutable
 import scala.jdk.CollectionConverters._
+import scala.util.Using
 
 object KafkaHomeWorkRunner extends App {
 
@@ -33,13 +35,16 @@ object KafkaHomeWorkRunner extends App {
 
   // First part: read csv file & serialize into JSON
   val filePathName = "src/main/resources/data/bestsellers_with_categories.csv"
-  // TODO: proper try catch
-  val in: Reader = new FileReader(filePathName)
-  // read the content of the file
-  val csvRecords: List[CSVRecord] = CSVFormat.RFC4180.builder()
-    .setHeader("Name,Author,User Rating,Reviews,Price,Year,Genre")
-    .setSkipHeaderRecord(true)
-    .build().parse(in).getRecords.asScala.toList
+  var csvRecords: List[CSVRecord] = List()
+  Using(new FileReader(filePathName)) {
+    in => {
+      // read the content of the file
+       csvRecords = CSVFormat.RFC4180.builder()
+        .setHeader("Name,Author,User Rating,Reviews,Price,Year,Genre")
+        .setSkipHeaderRecord(true)
+        .build().parse(in).getRecords.asScala.toList
+    }
+  }
 
   // convert it to the list of scala case class objects
   val bookDescriptions: List[BookDescription] = csvRecords.map(rec
@@ -50,8 +55,6 @@ object KafkaHomeWorkRunner extends App {
 
   // prepare content for kafka
   val contentForKafka: List[String] = bookDescriptions.map(r => r.asJson(bookDescriptionEncoder).toString())
-  // we need to know how many records we're going to write
-  val recWritten = contentForKafka.size
 
   // Second part: write the content into the books Apache Kafka topics
   val topicName = "books"
@@ -60,14 +63,23 @@ object KafkaHomeWorkRunner extends App {
   baseConfig.put("key.serializer", Class.forName("org.apache.kafka.common.serialization.StringSerializer"))
   baseConfig.put("value.serializer", Class.forName("org.apache.kafka.common.serialization.StringSerializer"))
 
-  //TODO try catch
-  val producer: Producer[String, String] = new KafkaProducer[String, String](baseConfig)
-  contentForKafka.foreach(rec => producer.send(new ProducerRecord[String, String](topicName, rec))) // use round robin partitioning
-  producer.flush()
-  producer.close()
-  println("Records written by producer: " + recWritten)
+  // we need to know how many records we're supposed to write
+  val recWritten = contentForKafka.size
+  Using (new KafkaProducer[String, String](baseConfig)){
+    producer => {
+      contentForKafka.foreach(rec => producer.send(new ProducerRecord[String, String](topicName, rec))) // use round robin partitioning
+      producer.flush()
+      println("Records written by producer: " + recWritten)
+    }
+  }
 
   // Third part: read it and print the last 5 from each of 3 partitions
+
+  val maxNumberOfRecordsPerPartition = 5
+  // here we store our last records per partition (not more than maxNumberOfRecordsPerPartition value for each partition)
+  val partitionMap: mutable.Map[Int, mutable.ArrayDeque[String]] = mutable.Map()
+  var recordProcessed = 0
+
   val consumerConfig: Properties = new Properties()
   consumerConfig.put("bootstrap.servers", "localhost:29092")
   consumerConfig.put("key.deserializer", Class.forName("org.apache.kafka.common.serialization.StringDeserializer"))
@@ -77,35 +89,43 @@ object KafkaHomeWorkRunner extends App {
   consumerConfig.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false")
   consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
 
-  val consumer: Consumer[String, String] = new KafkaConsumer[String, String](consumerConfig)
-  val topics = consumer.partitionsFor(topicName)
-  consumer.subscribe(Collections.singleton(topicName))
-
-  val maxNumberOfRecordsPerPartition = 5
-  var recordProcessed = 0
-  // here we store our last records per partition (not more than maxNumberOfRecordsPerPartition value)
-  val partitionMap: mutable.Map[Int, mutable.ArrayDeque[String]] = mutable.Map()
-
-  while (recordProcessed < recWritten) {
-    // read the portion of data and do the magic...remember we shouldn't store more than maxNumberOfRecordsPerPartition per 1 partition
-    consumer.poll(Duration.ofSeconds(1L)).forEach(
-      rec => {
-        recordProcessed += 1
-        val partition = rec.partition()
-        partitionMap get partition match {
-          case None =>
-            val deq: mutable.ArrayDeque[String] = mutable.ArrayDeque()
-            partitionMap += (partition -> deq)
-            deq.append(rec.value())
-          case Some(deq) =>
-            if (deq.size >= maxNumberOfRecordsPerPartition) {
-              deq.removeHead()
+  Using (new KafkaConsumer[String, String](consumerConfig)) {
+    consumer => {
+      consumer.subscribe(Collections.singleton(topicName))
+      while (recordProcessed < recWritten) {
+        // read the portion of data and do the magic...remember we shouldn't store more than maxNumberOfRecordsPerPartition per 1 partition
+        consumer.poll(Duration.ofSeconds(1L)).forEach(
+          rec => {
+            recordProcessed += 1
+            val partition = rec.partition()
+            partitionMap get partition match {
+              case None =>
+                val deq: mutable.ArrayDeque[String] = mutable.ArrayDeque()
+                partitionMap += (partition -> deq)
+                deq.append(rec.value())
+              case Some(deq) =>
+                if (deq.size >= maxNumberOfRecordsPerPartition) {
+                  deq.removeHead()
+                }
+                deq.append(rec.value())
             }
-            deq.append(rec.value())
-        }
+          }
+        )
       }
-    )
+    }
   }
+
   println("Records processed by consumer: " + recordProcessed)
-  println("Fin!", partitionMap)
+  partitionMap.foreach(
+    e => {
+      println("Partition #" + e._1 + ":")
+      e._2.foreach(rec => {
+        decode[BookDescription](rec) match {
+          case Right(bookDescription: BookDescription) => println(bookDescription)
+          case Left(ex) => println("Error while decoding json", ex)
+        }
+      })
+    }
+  )
+  println("Fin!")
 }

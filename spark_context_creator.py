7efe2ee6ca5c2d709c4ed1a98eb2bc3fc46e7692
andymurh <andymurh@gmail.com>
2021-09-22 09:20:10 +0200

    Example of spark context creation

---
+++ spark_context_creator.py
@@ -0,0 +1,14 @@
+def spark_context_creator():
+    conf = SparkConf()
+    conf.setAppName("AppName")
+    conf.setMaster('spark://localhost:7077')
+    sc = None
+    try:
+        sc.stop()
+        sc = SparkContext(conf=conf)
+    except:
+        sc = SparkContext(conf=conf)
+    return sc
+
+sc = spark_context_creator()
+sc.setLogLevel("WARN")

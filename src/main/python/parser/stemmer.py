996d14090f508da96e55a2a81e6389190a20c53a
andymurh <andymurh@gmail.com>
2021-09-26 19:10:26 +0200

    Remove main code

--- src/main/python/parser/stemmer.py
+++ src/main/python/parser/stemmer.py
@@ -31,17 +31,3 @@ def replace(original_line, rules):
         result_line = result_line.replace(rule[0], rule[1])
     return result_line
 
-if __name__ == "__main__":
-    lines = []
-    with open('./vacancies_raw_content.txt') as f:
-        for l in f:
-            lines.append(l)
-
-    stemmed_words = []
-
-    for line in lines:
-        line_words = word_tokenize(line)
-        for word in line_words:
-            stemmed_words.append(ps.stem(word))
-
-    print(stemmed_words)

d266d707917d3303c6ad670ae01f10bb65e7c833
andymurh <andymurh@gmail.com>
2021-09-27 20:36:34 +0200

    Better serialization/deserialization

--- src/main/python/parser/kafka_consumer_test.py
+++ src/main/python/parser/kafka_consumer_test.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3.7
 from kafka import KafkaConsumer
+from kafka import KafkaProducer
+import parser
+import json
 
 kafka_cluster_url = "localhost:29092"
 topic_name = "books"
+out_topic_name = "out_msg"
 
-consumer = KafkaConsumer(topic_name, bootstrap_servers=[kafka_cluster_url])
+tags_dict = parser.read_tags_dict_from_csv("./tech_tags_edited.csv")
+
+consumer = KafkaConsumer(topic_name, bootstrap_servers=[kafka_cluster_url], value_deserializer= json.loads)
+producer = KafkaProducer(bootstrap_servers=kafka_cluster_url, value_serializer=str.encode, key_serializer=str.encode)
+
 for msg in consumer:
     print(msg.value)
+    postlines = msg.value.split("\n")
+    (tags) = parser.parse(postlines, tags_dict, {}, {})
+    # remove first line / parse as date
+    event_stream_key = str(hash(msg.value))
+    event_stream_value = str(tags)
+    producer.send(out_topic_name, key = event_stream_key, value = event_stream_value)
+    producer.flush()
--- src/main/python/parser/kafka_producer_test.py
+++ src/main/python/parser/kafka_producer_test.py
@@ -1,17 +1,19 @@
-#https://towardsdatascience.com/connecting-the-dots-python-spark-and-kafka-19e6beba6404
+# https://towardsdatascience.com/connecting-the-dots-python-spark-and-kafka-19e6beba6404
 #!/usr/bin/python3.7
 
-import random
 import kafka
+from parser import stemmer
+from parser import reader
+import json
 
 kafka_cluster_url = "localhost:29092"
 topic_name = "books"
 
-producer = kafka.KafkaProducer(bootstrap_servers=kafka_cluster_url, value_serializer=str.encode, key_serializer=str.encode)
+producer = kafka.KafkaProducer(bootstrap_servers=kafka_cluster_url, value_serializer=lambda v: json.dumps(v).encode('utf-8'), key_serializer=str.encode)
 
 for ind in range(1, 10):
     event_stream_key = 'identifier_' + str(ind)
-    event_stream_value = 'bookval_' + str(random.randint(0, 100))
+    event_stream_value = reader.read_vacancy_raw_data('./raw_data.dat', ind)
     print(event_stream_key + ":" + event_stream_value)
     producer.send(topic_name, key = event_stream_key, value = event_stream_value)
     producer.flush()

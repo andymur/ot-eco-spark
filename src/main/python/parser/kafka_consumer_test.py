f4f1e2eaf0201bd9516fa89a32e8c727438390c5
andymurh <andymurh@gmail.com>
2021-09-28 08:10:11 +0200

    Other things related to parser

--- src/main/python/parser/kafka_consumer_test.py
+++ src/main/python/parser/kafka_consumer_test.py
@@ -11,14 +11,11 @@ out_topic_name = "out_msg"
 tags_dict = parser.read_tags_dict_from_csv("./tech_tags_edited.csv")
 
 consumer = KafkaConsumer(topic_name, bootstrap_servers=[kafka_cluster_url], value_deserializer= json.loads)
-producer = KafkaProducer(bootstrap_servers=kafka_cluster_url, value_serializer=str.encode, key_serializer=str.encode)
+#producer = KafkaProducer(bootstrap_servers=kafka_cluster_url, value_serializer=str.encode, key_serializer=str.encode)
 
 for msg in consumer:
-    print(msg.value)
     postlines = msg.value.split("\n")
+    # parse date, tags, city, country, salary
     (tags) = parser.parse(postlines, tags_dict, {}, {})
-    # remove first line / parse as date
-    event_stream_key = str(hash(msg.value))
-    event_stream_value = str(tags)
-    producer.send(out_topic_name, key = event_stream_key, value = event_stream_value)
-    producer.flush()
+    # remove first line as date 
+    print("message come: {0} with tags {1}".format(msg.value, str(tags)))
--- src/main/python/parser/parser.py
+++ src/main/python/parser/parser.py
@@ -4,20 +4,36 @@ import stemmer
 import reader
 import sys
 
-# parse (tech) tags, location (city, country), salary (range or number)
+# parse posted_at, (tech) tags, location (city, country), salary (range or number)
 def parse(postlines, tags_dict, cities_dict, lands_dict):
     stemmed_word_pairs = stemmer.stem_lines(postlines)
     tags = []
+    city = None
+    land = None
     for (original_word, stemmed_word) in stemmed_word_pairs:
         if stemmed_word in tags_dict:
             tags.append(original_word)
-    return (tags)
+        if city is None and original_word.lower() in cities_dict:
+            city = original_word
+        if city is None and land is None and original_word.lower() in lands_dict:
+            land = original_word
+    return (tags, city, land)
 
 def read_cities_from_csv(source_file):
-    pass
+    cities = []
+    with open(source_file) as f:
+        csvreader = csv.reader(f, delimiter = ";")
+        for row in csvreader:
+            cities.append(row[2].lower())
+    return cities
 
 def read_countries_from_csv(source_file):
-    pass
+    countries = []
+    with open(source_file) as f:
+        csvreader = csv.reader(f, delimiter = ";")
+        for row in csvreader:
+            countries.append(row[2].lower())
+    return countries
 
 def read_tags_dict_from_csv(source_file):
     tags_dict = {}
@@ -32,6 +48,9 @@ def read_tags_dict_from_csv(source_file):
 
 if __name__ == "__main__":
     tags_dict = read_tags_dict_from_csv("tech_tags_edited.csv")
+    countries = read_countries_from_csv("countries.csv")
+    cities = read_cities_from_csv("cities.csv")
+
     vacancy = reader.read_vacancy_raw_data("raw_data.dat", 0 if len(sys.argv) < 2 else int(sys.argv[1]))
     print(vacancy)
-    print(parse(vacancy.split("\n"), tags_dict, {}, {}))
+    print(parse(vacancy.split("\n"), tags_dict, cities, countries))

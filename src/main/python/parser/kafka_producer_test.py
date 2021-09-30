fddeabd4e28772096b74fd1060a5931f5d7492db
andymurh <andymurh@gmail.com>
2021-09-30 17:54:45 +0200

    Push all data to kafka from specific file

--- src/main/python/parser/kafka_producer_test.py
+++ src/main/python/parser/kafka_producer_test.py
@@ -11,9 +11,11 @@ topic_name = "in_msg"
 
 producer = kafka.KafkaProducer(bootstrap_servers=kafka_cluster_url, value_serializer=lambda v: json.dumps(v).encode('utf-8'), key_serializer=str.encode)
 
-for ind in range(0, 385):
+ind = 0
+for vac in reader.read_vacancies_raw_data("./raw_data.dat"):
+    ind += 1
     event_stream_key = 'identifier_' + str(ind)
-    event_stream_value = reader.read_vacancy_raw_data('./raw_data.dat', ind)
+    event_stream_value = vac
     print(event_stream_key + ":" + event_stream_value)
     producer.send(topic_name, key = event_stream_key, value = event_stream_value)
     producer.flush()

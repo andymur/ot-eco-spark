e398adbb5fbc26c506d7ae89a66ecbb8eb579895
andymurh <andymurh@gmail.com>
2021-10-04 21:15:46 +0200

    Delete old sources

--- kafka_recreate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./kafka-topics.sh --bootstrap-server localhost:29092 --delete --topic in_msg --if-exists
-./kafka-topics.sh --bootstrap-server localhost:29092 --delete --topic out_msg --if-exists
-./kafka-topics.sh --bootstrap-server localhost:29092 --create --topic in_msg
-./kafka-topics.sh --bootstrap-server localhost:29092 --create --topic out_msg

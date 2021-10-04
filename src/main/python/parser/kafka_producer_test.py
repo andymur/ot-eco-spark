56bc235bfd4f895a99d14e409dacf40f21235144
andymurh <andymurh@gmail.com>
2021-10-04 20:47:56 +0200

    Read from parameter

--- src/main/python/parser/kafka_producer_test.py
+++ src/main/python/parser/kafka_producer_test.py
@@ -5,6 +5,7 @@ import kafka
 from parser import stemmer
 from parser import reader
 import json
+import sys
 
 kafka_cluster_url = "localhost:29092"
 topic_name = "in_msg"
@@ -12,7 +13,8 @@ topic_name = "in_msg"
 producer = kafka.KafkaProducer(bootstrap_servers=kafka_cluster_url, value_serializer=lambda v: json.dumps(v).encode('utf-8'), key_serializer=str.encode)
 
 ind = 0
-for vac in reader.read_vacancies_raw_data("./raw_data.dat"):
+source_file = sys.argv[1] if len(sys.argv) > 1 else './raw_data.dat'
+for vac in reader.read_vacancies_raw_data(source_file):
     ind += 1
     event_stream_key = 'identifier_' + str(ind)
     event_stream_value = vac
